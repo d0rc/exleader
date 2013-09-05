efb67dee4e49f3fcfc464aa8264c6e9a844ba9c9
d0rc <7993863@gmail.com>
2013-09-06 02:19:57 +0400

    cleanup

--- lib/exleader.ex
+++ lib/exleader.ex
@@ -45,29 +45,3 @@ defmodule Exleader.Behaviour do
 		end
 	end
 end
-
-defmodule Messager do
-	use Exleader.Behaviour
-
-	def start_link, do: start_link([:erlang.node|:erlang.nodes])
-	def start_link(nodes), do: start_link(nodes)
-
-	def elected(state, _election, :undefined) do
-		IO.puts "Elected as leader, starting priting..."
-		Exleader.cast Messager, :print_time
-		{:ok, [], state}
-	end
-	def elected(state, _election, _node), do: {:ok, [], state}
-
-	def handle_leader_cast(:print_time, state, _election) do
-		IO.puts "Priting time"
-		:erlang.spawn_link fn() ->
-			receive do after 1000 -> :ok end
-			Exleader.cast Messager, :print_time
-		end
-		{:noreply, state}
-	end
-	def handle_leader_cast(_request, state, _election) do
-		{:noreply, state}
-	end
-end
